ef1d9e5970e11ba2395c4a3a56dd020fef90f27b
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2025-04-15 00:39:09 -0300

    testeErro

---
+++ prisma/migrations/20250415032903_create_veiculo_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Veiculo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "placa" TEXT NOT NULL,
+    "modelo" TEXT NOT NULL,
+    "cor" TEXT NOT NULL,
+    "ano" INTEGER NOT NULL
+);
