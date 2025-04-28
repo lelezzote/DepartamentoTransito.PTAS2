49b9d8218b436d164478957f37bcbb413900ce69
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2025-04-28 14:43:57 -0400

    teste2804

--- controllers/VeiculoController.js
+++ controllers/VeiculoController.js
@@ -1,11 +1,26 @@
 const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
 
 class VeiculoController {
     static formCadastro(req, res) {
         res.sendFile(path.join(__dirname, "..", "views", "formVeiculo.html"))
     }
 
-    static async cadastrar(req,res) {}
+    static async cadastrar(req,res) {
+      const veiculo = await prisma.veiculo.create({
+       
+        data: {
+          placa: req.body.placa,
+          modelo: req.body.modelo,
+          ano: parseInt(req.body.ano),
+          cor: req.body.cor,
+        }
+      });
+       res.send(`O veículo foi cadastrado sob o ID ${veiculo.id}`)
+    }
 
 
     static buscarTodos(req,res) {}
